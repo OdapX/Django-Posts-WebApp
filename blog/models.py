eced5492eb624d1a68f34ef0921db0d9e7b82ada
xvendor <yahya.benzha1@gmail.com>
2021-08-16 16:25:28 +0100

    commit22

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 # Create your models here.
 
 
@@ -10,6 +11,15 @@ class post(models.Model):
     content = models.TextField()
     date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='posts_pics')
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 and img.width > 300:
+            size = (300, 300)
+            img.thumbnail(size)
+            img.save(self.image.path)
 
     def get_absolute_url(self):
         return reverse('blog-detail', kwargs={'pk': self.pk})
